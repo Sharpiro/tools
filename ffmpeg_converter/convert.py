5ad71f2c5eaf1baba240d381ddaeed8242a4524f
sharpiro <dsharpbb09@gmail.com>
2018-07-01 15:55:07 -0400

    mp3 works from CD and prompts user input

--- ffmpeg_converter/convert.py
+++ ffmpeg_converter/convert.py
@@ -1,12 +1,23 @@
-import glob
+from glob import glob
 import os
+import sys
 from subprocess import call
 
+# print(sys.argv)
 workingDirectory = os.getcwd()
-files = glob.glob(f"{workingDirectory}/input/*")
+files = glob(f"{workingDirectory}/*.aac")
+files = files + glob(f"{workingDirectory}/*.m4a")
+files = files + glob(f"{workingDirectory}/*.wmv")
+if not files:
+    sys.exit("no audio files to convert")
+
+print(f"do you want to convert {len(files)} files in {workingDirectory}? (y/n)")
+res = input()
+if res != "y":
+    sys.exit("user exited")
 for inputFilePath in files:
     fileName = os.path.basename(inputFilePath)
-    outputFilePath = f"{workingDirectory}/output/{fileName}"
+    outputFilePath = f"{workingDirectory}/{fileName}"
     command = f"ffmpeg -i \"{inputFilePath}\" -aq 0 -vn \"{outputFilePath}"[:-4] + ".mp3\""
     print(f"converting '{fileName}''")
     print(command)
---
+++ shared/commands/mp3.cmd
@@ -0,0 +1,2 @@
+@echo off
+python %~dp0..\..\ffmpeg_converter\convert.py %*
