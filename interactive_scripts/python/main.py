a0311f1924b75510bfe4cd97748a3b0756014024
sharpiro <dsharpbb09@gmail.com>
2019-01-24 16:52:58 -0500

    adding func to multiply by adding and shifting

--- interactive_scripts/python/main.py
+++ interactive_scripts/python/main.py
@@ -7,6 +7,32 @@ import secrets
 
 irreduciblePolynomial = 0x11b
 
+def bits(n):
+    """
+    Generates the binary digits of n, starting
+    from the least significant bit.
+
+    bits(151) -> 1, 1, 1, 0, 1, 0, 0, 1 // 8 bits
+    """
+    while n:
+        yield n & 1
+        n >>= 1
+
+def double_and_add(n, x):
+    """
+    Returns the result of n * x, computed using
+    the double and add algorithm.
+    """
+    result = 0
+    addend = x
+
+    for bit in bits(n):
+        if bit == 1:
+            result += addend
+        addend <<= 1
+
+    return result
+
 def random(length):
     return list(secrets.token_bytes(length))
 
