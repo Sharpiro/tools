0fdca4ed3ab84ceae0001b70e25b444f9b9c3e07
sharpiro <dsharpbb09@gmail.com>
2019-01-30 23:03:13 -0500

    fade in/out and json clip validation

--- ffmpeg_converter/clip.py
+++ ffmpeg_converter/clip.py
@@ -11,18 +11,34 @@ def get_seconds(timestamp):
     return hours + minutes + seconds
 
 
-file = Path("clips.json").read_text()
+def validate_clips(clips):
+    for clip in clips:
+        if clip["name"] == "":
+            raise EnvironmentError("invalid json, 'name' was empty")
+        if clip["start"] == "":
+            raise EnvironmentError("invalid json, 'start' was empty")
+        if clip["stop"] == "":
+            raise EnvironmentError("invalid json, 'stop' was empty")
+        get_seconds(clip["start"])
+        get_seconds(clip["stop"])
+
+
+file = Path("test-clips.json").read_text()
 clips = json.loads(file)
+validate_clips(clips)
 for clip in clips:
     start_seconds = get_seconds(clip["start"])
     stop_seconds = get_seconds(clip["stop"])
     duration_seconds = stop_seconds - start_seconds
+    video_fade_out_start_frames = (duration_seconds - 3) * 30
+    audio_fade_out_start_seconds = (duration_seconds - 3)
+    fade_duration_frames = 3 * 30
     clipName = clip["name"]
     print(clipName, duration_seconds)
-    command = f"ffmpeg -ss {start_seconds} -i original.mp4 -t {duration_seconds} -y {clipName}.mp4".split(" ") #54 seconds
+    command = f"ffmpeg -ss {start_seconds} -i original.mp4 -t {duration_seconds} -y -vf fade=in:0:{fade_duration_frames},fade=out:{video_fade_out_start_frames}:{fade_duration_frames} -af afade=in:st=0:d=3,afade=out:st={audio_fade_out_start_seconds}:d=3 {clipName}.mp4".split(" ") #54 seconds
     print(command)
     print()
-    # call(command)
+    call(command)
 
 
 # ffmpeg -i input.mp4 -ss 00:00:00.0 -t 00:00:10.0 -y -vf fade=in:0:60,fade=out:240:30 -af afade=in:st=0:d=1,afade=out:st=5:d=5 slide_fade_in.mp4
