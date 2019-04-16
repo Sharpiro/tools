bc8302c1b7923fbf79d4f657cf43e4470e22cca0
sharpiro <dsharpbb09@gmail.com>
2019-04-16 16:18:12 -0400

    binary func truncates better/added tests

--- interactive_scripts/python/test.py
+++ interactive_scripts/python/test.py
@@ -1,6 +1,5 @@
 import math
 from main import binary
-
 # x = -255
 # # x = 152405818018898732
 # # bits = 64
@@ -36,16 +35,16 @@ from main import binary
 n = 16
 padding = 10 - (n*8) % 10
 
-# (size, value)
-id = (15, binary(99, size=15, sep=0))
-iteration_exponent = (0, binary(25, size=0, sep=0))
-group_index = (4, binary(15, size=4, sep=0))
-group_threshold =(4, binary(15, size=4, sep=0))
-group_count =(4, binary(15, size=4, sep=0))
-member_index = (4, binary(15, size=4, sep=0))
-member_threshold = (4, binary(15, size=4, sep=0))
-padded_share_value = (padding + (8 * n), "0"*(padding + (8 * n)))
-checksum = (25, binary(25, size=30, sep=0))
+# # (size, value)
+# id = (15, binary(99, size=15, sep=0))
+# iteration_exponent = (0, binary(25, size=0, sep=0))
+# group_index = (4, binary(15, size=4, sep=0))
+# group_threshold =(4, binary(15, size=4, sep=0))
+# group_count =(4, binary(15, size=4, sep=0))
+# member_index = (4, binary(15, size=4, sep=0))
+# member_threshold = (4, binary(15, size=4, sep=0))
+# padded_share_value = (padding + (8 * n), "0"*(padding + (8 * n)))
+# checksum = (25, binary(25, size=30, sep=0))
 
 
 # word1 = 1000
@@ -67,12 +66,20 @@ checksum = (25, binary(25, size=30, sep=0))
 # print(len(padded_share_value))
 # print(padded_share_value)
 # print(id[1])
-print(binary(99, size=15, sep=8))
-print(binary(0, size=5, sep=8))
-print(binary(0, size=4, sep=8))
-print(binary(1 - 1, size=4, sep=8))
-print(binary(1 - 1, size=4, sep=8))
-print(binary(0, size=4, sep=8))
-print(binary(3 - 1, size=4, sep=8))
+
+# print(binary(99, size=15, sep=8))
+# print(binary(0, size=5, sep=8))
+# print(binary(0, size=4, sep=8))
+# print(binary(1 - 1, size=4, sep=8))
+# print(binary(1 - 1, size=4, sep=8))
+# print(binary(0, size=4, sep=8))
+# print(binary(3 - 1, size=4, sep=8))
 
 # print(iteration_exponent)
+# 55210138423
+x =binary(-258, "be", size=31, sep=0)
+print(x)
+print(len(x))
+
+# data = [1,2,3,4,5,6,7,8,9,10]
+# print(data[-5:])
