978331f808fc40b15e84ed8039c810dd83971068
sharpiro <dsharpbb09@gmail.com>
2019-04-27 15:04:53 -0400

    json storage/buttons/formatting

--- json/webpack.prod.js
+++ json/webpack.prod.js
@@ -1,17 +1,34 @@
- const merge = require('webpack-merge');
- const common = require('./webpack.common.js');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
- module.exports = merge(common, {
-   mode: 'production',
-   plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Output Management',
-        template: 'src/index.html',
-        favicon: "src/favicon.ico",
-        inlineSource: '.(js|css|svg)$'
-    }),
-    new HtmlWebpackInlineSourcePlugin()
-],
- });
+module.exports = merge(common, {
+    mode: 'production',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            template: 'src/index.html',
+            favicon: "src/favicon.ico",
+            inlineSource: '.(js|css|svg)$'
+        }),
+        new HtmlWebpackInlineSourcePlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+});
