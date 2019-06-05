f96a08ff53811577411581315c5b2f9d3e855118
sharpiro <dsharpbb09@gmail.com>
2019-06-04 22:50:18 -0400

    onion harvester service

---
+++ onion_harvester/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo starting... >> /home/pi/test_service/onions/log.txt
+/home/pi/gitbase/mkp224o/mkp224o -n 1000 -B -d /home/pi/test_service/onions abc123
---
+++ onion_harvester/web.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/pi/test_service/onions
+/usr/bin/python3 -m http.server
