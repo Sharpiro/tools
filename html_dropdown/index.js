056f33c3894d8658587998d5924fe11d79508d92
sharpiro <dsharpbb09@gmail.com>
2019-06-13 12:52:30 -0400

    add html dropdown

---
+++ html_dropdown/index.js
@@ -0,0 +1,53 @@
+let items = ["https://abc123123.abc123123.com:8084/api/abc123123/abc123123/abc123123/abc123123/abc123123/abc123123/abc123123", "About", "Contact"]
+// let items = ["A", "About", "Contact"]
+let dropdown
+
+initialize()
+
+function initialize() {
+    dropdown = document.getElementById("myDropdown")
+    updateDropdown()
+}
+
+function updateDropdown() {
+    dropdown.innerHTML = ""
+    for (var item of items) {
+        var span = document.createElement("span")
+        span.onclick = optionClicked
+        span.innerText = item
+        dropdown.appendChild(span)
+    }
+}
+
+function openDropdown() {
+    dropdown.classList.toggle("show");
+}
+
+function clearDropdown() {
+    if (dropdown.classList.contains('show')) {
+        dropdown.classList.remove('show');
+    }
+}
+
+function optionClicked(event) {
+    if (!event.srcElement.innerText) return
+    testInput.value = event.srcElement.innerText
+}
+
+function onKeyInput() {
+    clearDropdown()
+}
+
+function addItem() {
+    if (!testInput.value) return
+
+    items.unshift(testInput.value)
+    items = [...new Set(items)]
+    updateDropdown()
+}
+
+window.onclick = function (event) {
+    if (!event.target.matches('#testInput')) {
+        clearDropdown()
+    }
+}
