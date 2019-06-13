bc8ef1cd496074d39a0d0922849f71b2748ef2c9
sharpiro <dsharpbb09@gmail.com>
2019-06-13 15:53:45 -0400

    convert html dropdown to web component

--- html_dropdown/index.js
+++ html_dropdown/index.js
@@ -1,53 +1,53 @@
-let items = ["https://abc123123.abc123123.com:8084/api/abc123123/abc123123/abc123123/abc123123/abc123123/abc123123/abc123123", "About", "Contact"]
+// let items = ["https://abc123123.abc123123.com:8084/api/abc123123/abc123123/abc123123/abc123123/abc123123/abc123123/abc123123", "About", "Contact"]
 // let items = ["A", "About", "Contact"]
-let dropdown
+// let dropdown
 
-initialize()
+// initialize()
 
-function initialize() {
-    dropdown = document.getElementById("myDropdown")
-    updateDropdown()
-}
+// function initialize() {
+//     dropdown = document.getElementById("myDropdown")
+//     updateDropdown()
+// }
 
-function updateDropdown() {
-    dropdown.innerHTML = ""
-    for (var item of items) {
-        var span = document.createElement("span")
-        span.onclick = optionClicked
-        span.innerText = item
-        dropdown.appendChild(span)
-    }
-}
+// function updateDropdown() {
+//     dropdown.innerHTML = ""
+//     for (var item of items) {
+//         var span = document.createElement("span")
+//         span.onclick = optionClicked
+//         span.innerText = item
+//         dropdown.appendChild(span)
+//     }
+// }
 
-function openDropdown() {
-    dropdown.classList.toggle("show");
-}
+// function openDropdown() {
+//     dropdown.classList.toggle("show");
+// }
 
-function clearDropdown() {
-    if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
-    }
-}
+// function clearDropdown() {
+//     if (dropdown.classList.contains('show')) {
+//         dropdown.classList.remove('show');
+//     }
+// }
 
-function optionClicked(event) {
-    if (!event.srcElement.innerText) return
-    testInput.value = event.srcElement.innerText
-}
+// function optionClicked(event) {
+//     if (!event.srcElement.innerText) return
+//     testInput.value = event.srcElement.innerText
+// }
 
-function onKeyInput() {
-    clearDropdown()
-}
+// function onKeyInput() {
+//     clearDropdown()
+// }
 
-function addItem() {
-    if (!testInput.value) return
+// function addItem() {
+//     if (!testInput.value) return
 
-    items.unshift(testInput.value)
-    items = [...new Set(items)]
-    updateDropdown()
-}
+//     items.unshift(testInput.value)
+//     items = [...new Set(items)]
+//     updateDropdown()
+// }
 
-window.onclick = function (event) {
-    if (!event.target.matches('#testInput')) {
-        clearDropdown()
-    }
-}
+// window.onclick = function (event) {
+//     if (!event.target.matches('#testInput')) {
+//         clearDropdown()
+//     }
+// }
