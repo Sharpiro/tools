3365a25c5dc14734a0d0201bf1366b7c544ef1b0
sharpiro <dsharpbb09@gmail.com>
2019-12-13 22:12:25 -0500

    del old readme

--- directory_differ/main.py
+++ directory_differ/main.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import shutil
 
 iphone_directory = Path(
-    "/run/user/1000/gvfs/gphoto2:host=Apple_Inc._iPhone_2e5a7a973f3ed78e2a02efcb648e905fb060f78f/DCIM")
+    "/run/user/1000/gvfs/gphoto2:host=Apple_Inc._iPhone_guid/DCIM")
 iphone_files = list(i for i in iphone_directory.glob("**/*") if i.is_file())
 
 destination_directory = Path("/home/sharpiro/Dropbox/pictures/camera_roll")
@@ -18,6 +18,7 @@ files_copied = 0
 for iphone_file in iphone_files:
     if destination_dict.get(iphone_file.name.lower()) == None:
         new_file_path = f"/home/sharpiro/Dropbox/pictures/camera_roll/{iphone_file.name}"
+        print(iphone_file.name)
         shutil.copy(iphone_file.absolute(), new_file_path)
         files_copied += 1
 
