8fbbbfd04086026297ba0885dd87cf722c8f7a46
sharpiro <dsharpbb09@gmail.com>
2020-03-29 17:05:42 -0400

    brain_freak rust wasm boilerplate

---
+++ brain_freak/src/lib.rs
@@ -0,0 +1,23 @@
+extern crate web_sys;
+mod utils;
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+macro_rules! log {
+  ( $( $t:tt )* ) => {
+      web_sys::console::log_1(&format!( $( $t )* ).into());
+  }
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    log!("Hello, brain_freak!");
+}
