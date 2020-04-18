c0241ed50bce56e0dcd447ddf1a2e578efc2a2e2
sharpiro <dsharpbb09@gmail.com>
2020-04-18 12:21:45 -0400

    add reset button

--- brain_freak/www/index.js
+++ brain_freak/www/index.js
@@ -1,32 +1,26 @@
 import { memory as sharedMem } from "brain_freak/brain_freak_bg";
 import { ProgramIterator } from "brain_freak";
 
-// let program = ",.>,.<[->+<]>.";
-// let program = ",.,.,.,.,.,.,.";
-let program = ",.,.,.,.,.,.,.";
-let input = [1, 2, 3, 4, 5, 6, 7];
+// const defaultProgram = ",.>,.<[->+<]>.";
+// const defaultProgram = ",.,.,.,.,.,.,.";
+const defaultProgram = ",.,.,.,.,.,.,.";
+const defaultInput = [1, 2, 3, 4, 5, 6, 7];
 const programJson = localStorage.getItem("program");
-if (programJson) {
-  program = programJson;
-}
+let program = programJson ? programJson : defaultProgram;
 const inputJson = localStorage.getItem("input");
-if (inputJson) {
-  input = JSON.parse(inputJson);
-}
+/** @type {number[]} */
+let input = inputJson ? JSON.parse(inputJson) : defaultInput;
 const memSize = 10;
 const outputCapacity = 10;
-let iterator = ProgramIterator.new(
-  program,
-  10,
-  outputCapacity,
-  new Uint8Array(input)
-);
-let memPointer = iterator.get_memory_ptr();
-let memory = new Uint8Array(sharedMem.buffer, memPointer, memSize);
-let inputPointer = iterator.get_input_ptr();
-let outputPointer = iterator.get_output_ptr();
 
-const memoryEl = document.getElementById("memoryEl");
+let states = /** @type {State[]} */ ([]);
+let stateIndex = 0;
+let lazyLoading = true;
+let outputPointer = 0;
+/** @type {Uint8Array} */ let memory;
+/** @type {ProgramIterator} */ let iterator;
+initialize();
+updatePage(states[0]);
 
 /** @type {HTMLInputElement} */
 const inputDataEl = (document.getElementById("inputDataEl"));
@@ -36,59 +30,32 @@ inputDataEl.value = input.join("");
 const programInputEl = (document.getElementById("programInputEl"));
 programInputEl.value = program;
 
-const programCodeEl = document.getElementById("programCodeEl");
-if (!programCodeEl) throw new Error();
-programCodeEl.innerHTML = program;
+function initialize() {
+  iterator = ProgramIterator.new(
+    program,
+    memSize,
+    outputCapacity,
+    new Uint8Array(input)
+  );
+  let memPointer = iterator.get_memory_ptr();
+  memory = new Uint8Array(sharedMem.buffer, memPointer, memSize);
+  let inputPointer = iterator.get_input_ptr();
+  outputPointer = iterator.get_output_ptr();
 
-const outputEl = document.getElementById("outputEl");
-if (!outputEl) throw new Error();
-
-const inputEl = document.getElementById("inputEl");
-if (!inputEl) throw new Error();
-
-const inputLen = iterator.get_input_len();
-const initialInput = [...(new Uint8Array(sharedMem.buffer, inputPointer, inputLen))];
-const initialState = {
-  command: "",
-  memory: Array.from(memory),
-  input: initialInput,
-  output: [],
-  thePointer: 0,
-  programCounter: 0
-};
-/** @type {State[]} */
-let states = [initialState];
-let stateIndex = 0;
-let lazyLoading = true;
-
-updateMemoryEl(initialState);
-updateProgramEl(initialState);
-updateInputEl(initialState);
-updateOutputEl(initialState);
-
-/**
- * @param {any} ev
- */
-window.onkeydown = ev => {
-  if (ev.key === "ArrowLeft") {
-    if (stateIndex === 0) return;
-    const state = states[--stateIndex];
-    updateMemoryEl(state);
-    updateProgramEl(state);
-    updateInputEl(state);
-    updateOutputEl(state);
-    console.log("command:", state.command);
-  }
-  else if (ev.key === "ArrowRight") {
-    const state = loadRight();
-    if (state) {
-      updateMemoryEl(state);
-      updateProgramEl(state);
-      updateInputEl(state);
-      updateOutputEl(state);
-    }
-  }
-};
+  const inputLen = iterator.get_input_len();
+  const initialInput = [...(new Uint8Array(sharedMem.buffer, inputPointer, inputLen))];
+  const initialState = {
+    command: "",
+    memory: Array.from(memory),
+    input: initialInput,
+    output: [],
+    thePointer: 0,
+    programCounter: 0
+  };
+  states = [initialState];
+  lazyLoading = true;
+  stateIndex = 0;
+}
 
 function loadRight() {
   let state;
@@ -126,7 +93,7 @@ function lazyLoadRight() {
   const command = iterator.next();
   if (!command) return undefined;
 
-  inputPointer = iterator.get_input_ptr();
+  const inputPointer = iterator.get_input_ptr();
   const inputLen = iterator.get_input_len();
   const outputLen = iterator.get_output_len();
   const input = [...(new Uint8Array(sharedMem.buffer, inputPointer, inputLen))];
@@ -144,46 +111,41 @@ function lazyLoadRight() {
   return state;
 }
 
+/** @param {{key:string}} ev */
+window.onkeydown = ev => {
+  if (ev.key === "ArrowLeft") {
+    if (stateIndex === 0) return;
+    const state = states[--stateIndex];
+    updatePage(state);
+    console.log("command:", state.command);
+  }
+  else if (ev.key === "ArrowRight") {
+    const state = loadRight();
+    if (state) {
+      updatePage(state);
+    }
+  }
+};
+
 updateButton.onclick = () => {
-  console.log("updated");
   program = programInputEl.value;
-  if (!programCodeEl) throw new Error();
-  programCodeEl.innerHTML = program;
   input = inputDataEl.value.split("").map(s => +s);
   localStorage.setItem("program", program);
   localStorage.setItem("input", JSON.stringify(input));
 
-  iterator = ProgramIterator.new(
-    program,
-    10,
-    outputCapacity,
-    new Uint8Array(input)
-  );
-  memPointer = iterator.get_memory_ptr();
-  memory = new Uint8Array(sharedMem.buffer, memPointer, memSize);
-  inputPointer = iterator.get_input_ptr();
-  outputPointer = iterator.get_output_ptr();
-
-  const inputLen = iterator.get_input_len();
-  const initialInput = [...(new Uint8Array(sharedMem.buffer, inputPointer, inputLen))];
-  const initialState = {
-    command: "",
-    memory: Array.from(memory),
-    input: initialInput,
-    output: [],
-    thePointer: 0,
-    programCounter: 0
-  };
-  states = [initialState];
-  lazyLoading = true;
-  stateIndex = 0;
-  updateMemoryEl(initialState);
-  updateProgramEl(initialState);
-  updateInputEl(initialState);
-  updateOutputEl(initialState);
-
+  initialize();
+  updatePage(states[0]);
   //@ts-ignore
   updateButton.focus();
+};
+
+resetButton.onclick = () => {
+  input = defaultInput;
+  programInputEl.value = program = defaultProgram;
+  inputDataEl.value = input.join("");
+  localStorage.clear();
+  initialize();
+  updatePage(states[0]);
 };
 
 debugButton.onclick = () => {
@@ -191,11 +153,21 @@ debugButton.onclick = () => {
 };
 
 /** @param {State} state */
+function updatePage(state) {
+  updateMemoryEl(state);
+  updateProgramEl(state);
+  updateInputEl(state);
+  updateOutputEl(state);
+}
+
+/** @param {State} state */
 function updateMemoryEl(state) {
   const arrDisplay = state.memory.join(", ");
   const spaces = 1 + state.thePointer * 3;
   const thePtrDisplay = " ".repeat(spaces) + "^";
   const display = `[${arrDisplay}]\n${thePtrDisplay}`;
+
+  const memoryEl = document.getElementById("memoryEl");
   if (!memoryEl) throw new Error();
   memoryEl.innerHTML = display;
 }
@@ -206,6 +178,8 @@ function updateProgramEl(state) {
   const spaces = state.programCounter * 2;
   const prgCounterDisplay = " ".repeat(spaces) + "^";
   const display = `${arrDisplay}\n${prgCounterDisplay}`;
+
+  const programCodeEl = document.getElementById("programCodeEl");
   if (!programCodeEl) throw new Error();
   programCodeEl.innerHTML = display;
 }
@@ -214,6 +188,8 @@ function updateProgramEl(state) {
 function updateInputEl(state) {
   const arrDisplay = state.input.join(", ");
   const display = `[${arrDisplay}]`;
+
+  const inputEl = document.getElementById("inputEl");
   if (!inputEl) throw new Error();
   inputEl.innerHTML = display;
 }
@@ -222,6 +198,8 @@ function updateInputEl(state) {
 function updateOutputEl(state) {
   const arrDisplay = state.output.join(", ");
   const display = `[${arrDisplay}]`;
+
+  const outputEl = document.getElementById("outputEl");
   if (!outputEl) throw new Error();
   outputEl.innerHTML = display;
 }
