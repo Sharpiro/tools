98629ff29e3dec14d0e902bbcd010d2d5d6a8a40
sharpiro <dsharpbb09@gmail.com>
2020-06-25 10:52:20 -0400

    move to build/run scripts

---
+++ bitcoin/bitcoin_run/build_pod.sh
@@ -0,0 +1,2 @@
+podman build -t tor -f tor.Dockerfile .
+podman build -t bitcoin_runner -f bitcoin.Dockerfile .
--- bitcoin/bitcoin_run/run_pod.sh
+++ bitcoin/bitcoin_run/run_pod.sh
@@ -1,6 +1,4 @@
-podman build -t tor -f tor.Dockerfile .
-podman build -t bitcoin_runner -f bitcoin.Dockerfile .
-
+podman pod rm -f bitcoin_pod
 podman pod create --name bitcoin_pod -p 8332:8332
 podman run -d --pod bitcoin_pod tor
 podman run -d --pod bitcoin_pod bitcoin_runner
